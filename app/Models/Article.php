6ec93b1ba8b2c704123ba2e77185b30ae4acc191
Luca Di Fazio <highliuk@gmail.com>
2022-03-10 11:49:39 +0100

    Add Model Relationships

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     /*
           ____             __ _           
@@ -29,6 +31,10 @@ class Article extends Model
 
     */
 
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
 
     /*
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,35 +9,65 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    /*
+          ____             __ _           
+         / ___|___  _ __  / _(_) __ _ ___ 
+        | |   / _ \| '_ \| |_| |/ _` / __|
+        | |__| (_) | | | |  _| | (_| \__ \
+         \____\___/|_| |_|_| |_|\__, |___/
+                                |___/      
+    */
+
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /*
+         ____      _       _   _                 
+        |  _ \ ___| | __ _| |_(_) ___  _ __  ___ 
+        | |_) / _ \ |/ _` | __| |/ _ \| '_ \/ __|
+        |  _ <  __/ | (_| | |_| | (_) | | | \__ \
+        |_| \_\___|_|\__,_|\__|_|\___/|_| |_|___/
+
+    */
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    /*
+            _   _   _        _ _           _            
+           / \ | |_| |_ _ __(_) |__  _   _| |_ ___  ___ 
+          / _ \| __| __| '__| | '_ \| | | | __/ _ \/ __|
+         / ___ \ |_| |_| |  | | |_) | |_| | ||  __/\__ \
+        /_/   \_\__|\__|_|  |_|_.__/ \__,_|\__\___||___/
+
+    */
+
+
+
+    /*
+         ____                            
+        / ___|  ___ ___  _ __   ___  ___ 
+        \___ \ / __/ _ \| '_ \ / _ \/ __|
+         ___) | (_| (_) | |_) |  __/\__ \
+        |____/ \___\___/| .__/ \___||___/
+                        |_|              
+    */
 }
