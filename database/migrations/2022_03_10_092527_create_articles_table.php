9f49f57dd8b11325f633f85eed46c6b01b5e8cfc
Luca Di Fazio <highliuk@gmail.com>
2022-03-10 10:34:16 +0100

    Add columns to migrations

--- database/migrations/2022_03_10_092527_create_articles_table.php
+++ database/migrations/2022_03_10_092527_create_articles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
@@ -9,7 +10,11 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->string('title');
+            $table->string('text');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
