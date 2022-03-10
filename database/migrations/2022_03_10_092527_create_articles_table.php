97ff5dc024efc49407e70cee334cace5eb1cc72a
Luca Di Fazio <highliuk@gmail.com>
2022-03-10 10:28:35 +0100

    Add models: Article, Category, Tag

---
+++ database/migrations/2022_03_10_092527_create_articles_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
